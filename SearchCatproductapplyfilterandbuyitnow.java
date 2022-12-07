e8b1d9f37ca303d1dd9bf66bc49fc3bff59b3f38
aartisb <aartibrahme@ymail.com>
2022-12-07 16:31:04 +0530

    Searchcatproductthenapplyfilterandthenbuyit

---
+++ SearchCatproductapplyfilterandbuyitnow.java
@@ -0,0 +1,17 @@
+package Pages;
+
+public class SearchCatproductapplyfilterandbuyitnow 
+{
+	public String searchicon ="search-desktop";
+	//Search function on home page is identified by using id attribute.
+	public String mainfilter = "st-select-menu-toggle";
+	//Web element is found by class name attribute
+	
+	public String options = "/html/body/div[3]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div/div/div[1]/ul/li[1]/div/label/div/div";
+	///html/body/div[3]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div/div/div[1]/ul/li[1]/div/label/input
+	//WebElement is found by Xpath selector
+	
+	public String sizefilter = "st-toolbox-item st-select-menu";
+	public String sizefilter2 = "//*[contains(text(),'Size ')]";
+		
+}
