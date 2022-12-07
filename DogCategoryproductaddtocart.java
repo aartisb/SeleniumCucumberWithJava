44b8ac66eca085e1856d6c709c55023b5b1fd814
aartisb <aartibrahme@ymail.com>
2022-12-07 16:28:07 +0530

    DogCategoryProducttoaddincart

---
+++ DogCategoryproductaddtocart.java
@@ -0,0 +1,16 @@
+package Pages;
+
+public class DogCategoryproductaddtocart
+{
+	public String menuexpandlink = "Details-menu-drawer-container";
+	//this path is of clicking on 
+	public String dogproductlink ="//*[contains(text(),'DOGS')]";
+	public String dogsubproduct = "//a[@href='/collections/dog-food-treats']";
+	public String addtocart ="//div[contains(text(),'Add To Cart ')]";
+	public String checkoutbtn = "//*[contains(text(),'Checkout')]";
+	public String productclick = "prorep1";
+	public String addbtnonproductdetailpage = "add";
+	//public String chaticon = "shopify-chat-dummy";
+	public String clickoncheckout1 = "//form[@action='/cart']//button[@name='checkout' and @type='submit']";
+	
+}
